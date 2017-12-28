14c3bd2944e84b0604a490953afeb5d8f31c4bc5
satyaki-mitra <satyaki.mitra93@gmail.com>
2017-12-28 12:50:04 +0530

    First Commit

--- exercise_2/python_code/bitcoin-cross.py
+++ exercise_2/python_code/bitcoin-cross.py
@@ -3,6 +3,8 @@ import pandas as pd
 data = pd.read_csv('input_csv/bitcoin-cross-new.csv', sep = ',')
 df = data.set_index('#')
 
+## Largest exchnages in the given data ##
+
 df1 = df[['Source', 'Pair', 'Volume (24h)']]  
 # df1 is a slice of the main dataframe(df) containing only 'Source', 'Pair' and 'Volume(24h)' columns.
 df2 = df1.groupby(['Source']).sum()           
@@ -12,4 +14,25 @@ df3 = df2.sort_values(by = ['Volume (24h)'], ascending = False).head(25)
 df3.to_csv('output_csv/top25_exchanges.csv')
 
 
+
+## Most traded pairs in the given data ##
+
+df7 = pd.DataFrame(df.Pair.value_counts())
+# df7 is a dataframe which contains the 'Pair' and the frequency of the pairs.
+df8 = df7.rename(columns = {'Pair' : 'No_of_traded_pair'})
+df8.index.name = 'Pair'
+# df8 is another dataframe where we change the names of the index column to 'Pair' and 'Pair column to 'No_of_traded_pairs'.
+df8.head(10).to_csv('output_csv/most_traded_pairs.csv')
+
+
+
+## Cheapest pairs in the given data ##
+
+df4 = df[['Pair', 'Price']]  
+# df4 is a slice of the main dataframe(df) containing only 'Pair' and 'Price' columns.
+df5 = df4.groupby(['Pair']).sum()           
+# df5 is another dataframe which is grouped by 'Pair' and contains the total for a group object.
+df6 = df5.sort_values(by = ['Price'], ascending = True).head(25)
+# df6 is another dataframe which contains sorted 25 values of 'Price' in ascending order.
+df6.to_csv('output_csv/cheapest_25_pairs.csv')
 
