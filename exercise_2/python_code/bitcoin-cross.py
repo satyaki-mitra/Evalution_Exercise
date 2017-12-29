bc357a224791aae7aa6dc65642edabca4e5694e0
satyaki-mitra <satyaki.mitra93@gmail.com>
2017-12-29 16:37:29 +0530

    third commit

--- exercise_2/python_code/bitcoin-cross.py
+++ exercise_2/python_code/bitcoin-cross.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = pd.read_csv('input_csv/bitcoin-cross-new.csv', sep = ',')
 df = data.set_index('#')
 
-## Largest and smallest exchnages in the given data ##
+## Largest  & Smallest Exchnages wrt turnover in the given data ##
 
 df1 = df[['Source', 'Pair', 'Volume (24h)']]  
 # df1 is a slice of the main dataframe(df) containing only 'Source', 'Pair' and 'Volume(24h)' columns.
@@ -17,7 +17,6 @@ df4.to_csv('output_csv/bottom20_exchanges_wrt_turnover.csv')
 
 
 
-
 ## Largest  & Smallest Exchnages wrt traded pairs in the given data ##
 
 df15 = pd.DataFrame(df.Source.value_counts())
@@ -25,12 +24,12 @@ df15 = pd.DataFrame(df.Source.value_counts())
 df16 = df15.rename(columns = {'Source' : 'No_of_traded_pair'})
 df16.index.name = 'Source'
 # df16 is another dataframe where we change the names of the index column to 'Pair' and 'Pair column to 'No_of_traded_pairs'.
-df16.head(10).to_csv('output_csv/top20_exchanges_wrt_traded_pairs.csv')
-df16.tail(10).to_csv('output_csv/bottom20_exchnages_wrt_traded_pairs.csv')
+df16.head(20).to_csv('output_csv/top20_exchanges_wrt_traded_pairs.csv')
+df16.tail(20).to_csv('output_csv/bottom20_exchanges_wrt_traded_pairs.csv')
 
 
 
-## Most & least traded pairs wrt Exchanges in the given data ##
+## Cheapest & Expensive pairs in the given data ##
 
 df5 = df[['Pair', 'Price']]  
 # df5 is a slice of the main dataframe(df) containing only 'Pair' and 'Price' columns.
@@ -45,7 +44,7 @@ df8.to_csv('output_csv/expensive_20_pairs.csv')
 
 
 
-## Cheapest pairs in the given data ##
+## Most traded pairs  and Least traded pairs wrt Exchanges in the given data ##
 
 df9 = pd.DataFrame(df.Pair.value_counts())
 # df9 is a dataframe which contains the 'Pair' and the frequency of the pairs.
@@ -57,17 +56,14 @@ df10.tail(10).to_csv('output_csv/least_traded_pairs_wrt_exchanges.csv')
 
 
 
+## Most traded and least traded pairs wrt turnover in the given data ##
 
-## Most traded pairs wrt Turnover in the given data ##
-
-df9 = df[['Pair', 'Volume (24h)']]
-# df9 is a Dataframe that contains only 'Pair' & 'Volume (24h)' columns.
-
-df10 = df9.groupby('Pair').sum()
-# df10 is another Dataframe that makes the groups according to the 'Pair' and then takes sum of the 'Volume (24h)' of the corresponding groups. 
-
-df11 = df10.sort_values(by = ['Volume (24h)'], ascending = False).head(10)
-# df11 is a Dataframe that sort the 'Volume (24h)' column in descending order.
-
-df11.to_csv('output_csv/most_traded_pairs_wrt_turnover.csv')
-df11
+df11 = df[['Pair', 'Volume (24h)']]
+# df11 is a Dataframe that contains only 'Pair' & 'Volume (24h)' columns.
+df12 = df11.groupby('Pair').sum()
+# df12 is another Dataframe that makes the groups according to the 'Pair' and then takes sum of the 'Volume (24h)' of the corresponding groups. 
+df13 = df12.sort_values(by = ['Volume (24h)'], ascending = False).head(10)
+df14 = df12.sort_values(by = ['Volume (24h)'], ascending = True).head(10)
+# df12 & df14 are Dataframes that contains the top10 and bottom10 of the sorted 'Volume (24h)' column.
+df13.to_csv('output_csv/most_traded_pairs_wrt_turnover.csv')
+df14.to_csv('output_csv/least_traded_pairs_wrt_turnover.csv')
