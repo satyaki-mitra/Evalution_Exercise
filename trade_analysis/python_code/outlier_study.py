bdb9c35eea5178a1021ab4005872786ce323abc3
satyaki-mitra <satyaki.mitra93@gmail.com>
2018-02-02 18:08:04 +0530

    outlier_study

--- trade_analysis/python_code/outlier_study.py
+++ trade_analysis/python_code/outlier_study.py
@@ -3,15 +3,14 @@ import json
 import datetime
 
 
+
 def data_manipulation(df_in):
-    trade_data1 = data2[df_in]
-    trade_data = pd.DataFrame(trade_data1)
-    trade_data['datetime'] = pd.to_datetime(trade_data['date'])
-    trade_data['price'] = trade_data['price'].astype(float)
-    trade_data['amount'] = trade_data['amount'].astype(float)
-    trade_data['amountBase'] = trade_data['amountBase'].astype(float)
-    dataframe = trade_data[['datetime', 'amount', 'amountBase', 'price']].set_index('datetime')
-    return dataframe
+    df_in['datetime'] = pd.to_datetime(df_in['date'])
+    df_in['price'] = df_in['price'].astype(float)
+    df_in['amount'] = df_in['amount'].astype(float)
+    df_in['amountBase'] = df_in['amountBase'].astype(float)
+    required_dataframe = df_in[['datetime', 'amount', 'amountBase', 'price']].set_index('datetime')
+    return required_dataframe
 
 
 
@@ -22,42 +21,39 @@ def detect_outlier(input_df, col_name):
     lower_bound = q1 - (1.5 * iqr)
     upper_bound = q3 + (1.5 * iqr)
     df_out = input_df[(input_df[col_name] < lower_bound) | (input_df[col_name] > upper_bound)]
-    row1, col1 = df_out.shape
-    total_data = row1
     return df_out
 
 
 
-l = []
-def proportion_outlier(input_data1, input_data2):
+summary = []
+def summary_of_outliers(input_data1, input_data2):
     row1, col1 = input_data1.shape
     total_data = row1
     row2, col2 = input_data2.shape
     outliers_in_data = row2
     percentage_of_outliers = (float(row2)/float(row1))*100
     output = [i, total_data, outliers_in_data, percentage_of_outliers]
-    l.append(output)
-    return l
+    summary.append(output)
+    return summary
 
 
 
-data1 = pd.read_csv('output_files/initial_study/top__daily_traded_pairs.csv')
-pairs = list(data1['pair'])
-for i in pairs:
+top_hourly_traded_pairs = pd.read_csv('output_files/initial_study/top_hourly_traded_pairs.csv')
+pair_list = list(top_hourly_traded_pairs['pair'])
+for i in pair_list:
     fp = open("input_csv/ed_trade_data.json")
-    data2 = json.load(fp)
-    if (type(data2) != 'DataFrame'):
-        data = data_manipulation(i)
-    else:
-    	data = data2
-    outliers = detect_outlier(data, 'price')
-    outliers.to_csv('output_files/outlier_study/outliers_'+i+'.csv')
-    output_data = proportion_outlier(data, outliers)
+    trade_db = json.load(fp)
+    current_trade_data = trade_db[i]
+    current_dataframe = pd.DataFrame(current_trade_data)
+    manipulated_data = data_manipulation(current_dataframe)
+    outliers = detect_outlier(manipulated_data, 'price')
+    outliers.to_csv('output_files/outlier_study/OutlierReport_'+i+'.csv')
+    outlier_summary = summary_of_outliers(manipulated_data, outliers)
 
-report = pd.DataFrame(output_data)
+report = pd.DataFrame(outlier_summary)
 report.columns = ['pair', 'total_data', 'outliers_in_data', '%_of_outliers']
-outlier_report = report.sort_values(by = ['%_of_outliers'], ascending = True).set_index('pair')
-outlier_report.to_csv('output_files/outlier_study/outlier_report.csv')
+outlier_summary_report = report.sort_values(by = ['%_of_outliers'], ascending = True).set_index('pair')
+outlier_summary_report.to_csv('output_files/outlier_study/outlier_summary_report.csv')
 
 
 
