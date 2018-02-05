ac1140b123de9eb8eecdbb8064c03c82c4b0389d
satyaki-mitra <satyaki.mitra93@gmail.com>
2018-02-05 17:57:58 +0530

    outlier_study

--- trade_analysis/python_code/outlier_study.py
+++ trade_analysis/python_code/outlier_study.py
@@ -2,9 +2,12 @@ import pandas as pd
 import json
 import datetime
 
+fp = open("input_csv/ed_trade_data.json")
+TradeDb = json.load(fp)
 
 
-def data_manipulation(df_in):
+
+def DataManipulation(df_in):
     ''' This function is for converting object type columns to numeric type columns of the dataframe which is 
     passed as the argument here and returning the required dataframe with all numeric columns. ''' 
     
@@ -17,7 +20,7 @@ def data_manipulation(df_in):
 
 
 
-def detect_outlier(input_df, col_name):
+def DetectOutlier(input_df, col_name):
     ''' This function is taking a dataframe & one of its column as arguments and then calculating the 
     1st and 3rd quartiles. iqr is the variable where it's calculating the Inter Quartile Range and hence 
     it's calculating the upper and lower bound and checking  whether all the values of that particular
@@ -33,39 +36,49 @@ def detect_outlier(input_df, col_name):
 
 
 
-def percentage_of_outliers(list1, database):
+def ProcessPairlist(database, next_pair):
+    ''' This function is taking a database and one of the top traded pairs as arguments and then calling
+    another function DataManipulation to make the readymade dataframe and then calling another function 
+    DetectOutliers that takes the resultant dataframe as the argument of the previous function to detect 
+    the outliers in that dataframe. '''
+    
+    TradeData = database[next_pair]
+    TradeDataframe = pd.DataFrame(TradeData)
+    manipulated_data = DataManipulation(TradeDataframe)
+    outliers = DetectOutlier(manipulated_data, 'price')
+    outliers.to_csv('output_files/outlier_study/OutlierReport_'+ next_pair +'.csv')
+    return outliers
+
+
+
+def PrepareSummary(pair_list, outlier_files_path, database):
     ''' This function is determining the percentage of the outliers. For that it's first determining the shape
     of the actual dataframe and the dataframe of outliers, the list of pairs is passed as arguments in this. 
-    Then it's determining amount of data of those two dataframes. Then it's calculating the relative percentage 
+    Then it's determining amount of data in those two dataframes. Then it's calculating the relative percentage 
     of the outliers in the dataframe for each pair of coins and reurning a dataframe containing the summary. '''
+   
     info_of_outliers = []
-    for p in list1:
+    for p in pair_list:
         trade_data = database[p]
         trade_dataframe = pd.DataFrame(trade_data)
         row1, col1 = trade_dataframe.shape
-        outlier_data = pd.read_csv('output_files/outlier_study/OutlierReport_'+ p +'.csv')
+        outlier_data = pd.read_csv(outlier_files_path + p +'.csv')
         outlier_dataframe = pd.DataFrame(outlier_data)
         row2, col2 = outlier_dataframe.shape
         relative_percentage = (float(row2)/float(row1)) * 100
         summary1 = [p, row1, row2, relative_percentage]
         info_of_outliers.append(summary1)
     report_of_outliers = pd.DataFrame(info_of_outliers).sort_values(by = 2)
-    return report_of_outliers
+    report_of_outliers.columns = ['pair', 'total_data', 'outliers_in_data', '%_of_outliers']
+    summary_report = report_of_outliers.set_index('pair')
+    return summary_report
 
 
 
 top_hourly_traded_pairs = pd.read_csv('output_files/initial_study/top_hourly_traded_pairs.csv')
-pair_list = list(top_hourly_traded_pairs['pair'])
-fp = open("input_csv/ed_trade_data.json")
-trade_db = json.load(fp)
-for i in pair_list:
-    trade_data = trade_db[i]
-    trade_dataframe = pd.DataFrame(trade_data)
-    manipulated_data = data_manipulation(trade_dataframe)
-    outliers = detect_outlier(manipulated_data, 'price')
-    outliers.to_csv('output_files/outlier_study/OutlierReport_'+i+'.csv')
-
-report = percentage_of_outliers(pair_list, trade_db)
-report.columns = ['pair', 'total_data', 'outliers_in_data', '%_of_outliers']
-summary_report = report.set_index('pair')
-summary_report.to_csv('output_files/outlier_study/summary_report_of_outliers.csv')
+PairList = list(top_hourly_traded_pairs['pair'])
+for pair in PairList:
+    ProcessPairlist (TradeDb, pair)
+path_to_outliers = 'output_files/outlier_study/OutlierReport_'  
+SummaryReport = PrepareSummary(PairList, path_to_outliers, TradeDb)
+SummaryReport.to_csv('output_files/outlier_study/OutlierSummaryReport.csv')
