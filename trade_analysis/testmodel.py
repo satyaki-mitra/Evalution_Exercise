16b1c6e9f57acd3d8c9ee97931e103a8ed030187
satyaki-mitra <satyaki.mitra93@gmail.com>
2018-02-20 18:59:41 +0530

    model.py

--- trade_analysis/testmodel.py
+++ trade_analysis/testmodel.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import json
 import datetime
-import numpy as np
 from model import Pair, Trade
 fp = open("input_csv/ed_trade_data.json")
 jsondata = json.load(fp)
 pairlist = jsondata.keys()
 
-total_info = []
+total_data = []
 for i in pairlist:
-    pair_name = Pair().get_name(i)
-    pair_data = jsondata[str(pair_name)]
+    pair = Pair(i)
+    pair_data = jsondata[str(pair)]
+    trades = []
     for j in pair_data:
         trade = Trade(j)
-        trade_info = (pair_name, trade)
-        total_info.append(trade_info)
-print total_info
-    
+        trades.append(trade)
+    info_trade = (pair, trades)
+    total_data.append(info_trade)  
